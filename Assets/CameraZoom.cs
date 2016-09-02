ebf09595da9a7334497350365d5da36eed86f2e3
Minh <minh.nguyen1@uoit.net>
2016-09-01 22:12:44 -0400

    Initital Commit

---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+    // Use this for initialization
+    public SteamVR_TrackedObject rightController;
+    public Camera scopeCamera;
+    public Light spotLight;
+
+    public int iter = 10;
+    public const float minInt = 0.0f;
+    public const float maxInt = 3.0f;
+    public const float minFOV = 1.0f;
+    public const float maxFOV = 10.0f;
+
+	void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var device = SteamVR_Controller.Input((int)rightController.index);
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            float touchY = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).y;
+            float fov = scopeCamera.fieldOfView - touchY*0.05f;
+            float touchX = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).x;
+            float intensity = spotLight.intensity + touchX*0.05f;
+
+            if (fov < minFOV)
+                scopeCamera.fieldOfView = minFOV;
+            else if (fov > maxFOV)
+                scopeCamera.fieldOfView = maxFOV;
+            else
+                scopeCamera.fieldOfView = fov;
+
+            if (intensity < minInt)
+                spotLight.intensity = minInt;
+            else if (intensity > maxInt)
+                spotLight.intensity = maxInt;
+            else
+                spotLight.intensity = intensity;
+           
+        }
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Grip))
+        {
+            iter -= 1;
+        }
+        else
+        {
+            iter = 5;
+        }
+        
+        
+	}
+}
---
+++ Assets/OnEyeCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnEyeCollision : MonoBehaviour {
+
+    public GameObject pupil;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        pupil.SetActive(false);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        pupil.SetActive(true);
+    }
+}
